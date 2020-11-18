d8697e1333f9a078c9d4de455ec6fce547165485
Rasta Alex <rastaalex86@gmail.com>
2020-11-18 19:05:34 +0200

    New changes

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const test = require('tape');
+const tryCatch = require('try-catch');
 const hex2dec = require('..');
 
 test('hex2dec: valid hex',(t) => {
@@ -11,3 +12,17 @@ test('hex2dec: valid hex',(t) => {
     t.end();
 })
 
+test('hex2dec: empty arguments', (t) => {
+    const [error] = tryCatch(hex2dec);
+
+    t.equal(error.message, 'hex should be string!');
+    t.end();
+});
+
+test ('hex2dec: empty string', (t) => {
+     const actual = hex2dec('');
+     const expect = 'Error: hex should be string!';
+
+     t.deepEqual(actual, expect);
+     t.end();
+ })
